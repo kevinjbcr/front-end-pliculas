4361cde19e8bf22f725032dd018a204797585133
Kevin <jjimenezbal@gmail.com>
2022-12-03 19:19:57 -0600

    Leccion 50

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { primeraLetraMayuscula } from 'src/app/utilidades/validadores/primeraLetraMayuscula';
 
 @Component({
   selector: 'app-crear-genero',
@@ -13,7 +14,7 @@ export class CrearGeneroComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      nombre: ['', { validators: [Validators.required, Validators.minLength(3)] }]
+      nombre: ['', { validators: [Validators.required, Validators.minLength(3), primeraLetraMayuscula()] }]
     })
   }
 
@@ -32,6 +33,10 @@ export class CrearGeneroComponent implements OnInit {
 
     if (campo.hasError('minlength')) {
       return 'La longitud mínima es de 3 caracteres'
+    }
+
+    if (campo.hasError('primeraLetraMayuscula')) {
+      return campo.getError('primeraLetraMayuscula').mensaje
     }
 
     return ''
