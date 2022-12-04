b4994a8bbc2c7636a1e313cba9f8fbcb4f3e71f2
Kevin <jjimenezbal@gmail.com>
2022-12-03 21:31:36 -0600

    Leccion 53

--- src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
+++ src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -8,26 +8,58 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class FiltroPeliculasComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder) { }
 
   form: FormGroup
 
   generos = [
-    {id: 1, nombre: 'Drama'},
-    {id: 2, nombre: 'Acción'},
-    {id: 3, nombre: 'Comedia'},
+    { id: 1, nombre: 'Drama' },
+    { id: 2, nombre: 'Acción' },
+    { id: 3, nombre: 'Comedia' },
   ]
 
+  peliculas = [
+    { titulo: 'Dragon Ball', enCines: false, proximosEstrenos: false, generos: [1, 2], poster: 'https://cdn.pixabay.com/photo/2022/08/07/07/50/skateboard-7370081__340.png' },
+    { titulo: 'Naruto', enCines: true, proximosEstrenos: true, generos: [3], poster: 'https://cdn.pixabay.com/photo/2022/08/07/07/50/skateboard-7370081__340.png' },
+    { titulo: 'One Pice', enCines: true, proximosEstrenos: false, generos: [1, 3], poster: 'https://cdn.pixabay.com/photo/2022/08/07/07/50/skateboard-7370081__340.png' },
+  ]
+
+  peliculasOriginal = this.peliculas
+
+  formularioOriginal = {
+    titulo: '',
+    generoId: 0,
+    proximosEstrenos: false,
+    enCines: false
+  }
+
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      titulo: '',
-      generoId: 0,
-      proximosEstrenos: false,
-      enCines: false
+    this.form = this.formBuilder.group(this.formularioOriginal)
+
+    this.form.valueChanges.subscribe(valores => {
+      this.peliculas = this.peliculasOriginal
+      this.buscarPeliculas(valores)
     })
   }
 
-  limpiar(){}
+  buscarPeliculas(valores: any) {
+    if (valores.titulo) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.titulo.indexOf(valores.titulo) !== -1)
+    }
+    if (valores.generoId !== 0) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.generos.indexOf(valores.generoId) !== -1)
+    }
+    if (valores.enCines) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.enCines)
+    }
+    if (valores.proximosEstrenos) {
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.proximosEstrenos)
+    }
+  }
+
+  limpiar() { 
+    this.form.patchValue(this.formularioOriginal)
+  }
 
 
 
